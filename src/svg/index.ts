65ac3fc0c91d462c23c5c40675087deded8a1348
Vera Krasnova <azirafail@mail.ru>
2022-12-12 16:41:01 +0300

    layout/donation: add donation section

--- src/svg/index.ts
+++ src/svg/index.ts
@@ -3,5 +3,6 @@ import { ReactComponent as FacebookLogo } from "./socials/facebook.svg";
 import { ReactComponent as InstagramLogo } from "./socials/instagram.svg";
 import { ReactComponent as TwitterLogo } from "./socials/twitter.svg";
 import { ReactComponent as YouTubeLogo } from "./socials/youtube.svg";
+import { ReactComponent as Panda } from "./panda.svg";
 
-export { Logo, FacebookLogo, TwitterLogo, InstagramLogo, YouTubeLogo };
+export { Logo, FacebookLogo, TwitterLogo, InstagramLogo, YouTubeLogo, Panda };
