766c9ea5a44ac6d41bd60fe164db96c5046feaa1
Vera Krasnova <azirafail@mail.ru>
2022-12-20 17:27:09 +0300

    adaptivity: implement burger menu

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,9 @@
-import { Header } from "./Header/Header";
-import { Button } from "./Button/Button";
-import { Footer } from "./Footer/Footer";
-import { SubscribeForm } from "./SubscribeForm/SubscribeForm";
-import { TestimonialCard } from "./TestimonialCard/TestimonialCard";
-import { AnimalCard } from "./AnimalCard/AnimalCard";
+import { Header } from './Header/Header';
+import { Button } from './Button/Button';
+import { Footer } from './Footer/Footer';
+import { SubscribeForm } from './SubscribeForm/SubscribeForm';
+import { TestimonialCard } from './TestimonialCard/TestimonialCard';
+import { AnimalCard } from './AnimalCard/AnimalCard';
+import { Burger } from './Burger';
 
-export { Header, Button, Footer, SubscribeForm, TestimonialCard, AnimalCard };
+export { Header, Button, Footer, SubscribeForm, TestimonialCard, AnimalCard, Burger };
